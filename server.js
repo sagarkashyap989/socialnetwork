9e7fc5b05a5bbc4837878c103f32e7e699f934b3
sagarkashyap989 <75683778+sagarkashyap989@users.noreply.github.com>
2022-06-20 17:31:18 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+// const mongoose = require("mongoose");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// connect to database
+connectDB();
+
+// Init Middleware
+app.use( express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API runing"));
+
+ 
+//Define routes
+app.use("/api/user", require("./routes/api/user"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/post", require("./routes/api/post"));
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
